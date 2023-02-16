ceb066d650820abbea3c0262cd65313a65834fb3
pavloyurchyshyn <payurchyshyn@gmail.com>
2023-02-16 21:01:14 +0200

    compiling

--- compiler.py
+++ compiler.py
@@ -1,56 +1,53 @@
 import os
-import re
 import shutil
 import subprocess
-import auto_py_to_exe
+import datetime
 from pathlib import Path
-
-
-def increase_version(versions: list):
-    versions = versions.copy()
-
-    v = versions[-1] + 1
-    if v > 99:
-        v = v - 100
-        versions[-2] += 1
-    versions[-1] = v
-    if versions[-2] > 100:
-        versions[-2] = versions[-2] - 100
-        versions[0] += 1
-
-    return versions
+from settings.version import VERSION
 
 
 if __name__ == '__main__':
     root = Path(__file__).parent
     out_folder = root / 'compiled_versions'
-    version_file = root / 'settings' / 'version.py'
     launch = root / 'launch.py'
     config = root / 'compile_conf.json'
 
-    dest_folder = root / 'dist'
+    dist_folder = root / 'dist'
+    build_folder = root / 'build'
+    launch_spec_file = root / 'launch.spec'
+    server_spec_file = root / 'server.spec'
+    try:
+        if not out_folder.exists():
+            out_folder.mkdir()
 
-    if not out_folder.exists():
-        out_folder.mkdir()
+        compiled_out = Path(out_folder, f'v{VERSION}_{datetime.datetime.now().strftime("%m_%d_%Y_%H_%M")}')
+        # args = ['auto-py-to-exe',
+        #         launch.as_posix(),
+        #         '-c', config.as_posix(),
+        #         '--output-dir', compiled_out.as_posix(),
+        #         ]
+        # subprocess.call(args)
+        subprocess.call('pyinstaller '
+                        '--noconfirm --onedir --windowed '
+                        '--add-data "E:/nastilna_gra_refactor/localization;localization/" '
+                        '--add-data "E:/nastilna_gra_refactor/sounds;sounds/" '
+                        '--add-data "E:/nastilna_gra_refactor/textures;textures/"  '
+                        '"E:/nastilna_gra_refactor/launch.py"')
 
-    version = version_file.read_text()
-    version = version.split("'")[-2]
-    vers = list(map(int, version.split('.')))
-    vers = increase_version(vers)
-    str_version = '.'.join(map(str, vers))
-
-    version_file.write_text(f"VERSION = '{str_version}'\n")
-    compiled_out = Path(out_folder, f'v{str_version}')
-    args = ['pyinstaller',
-            '--noconfirm', '--onedir', '--windowed',
-            '--add-data', '"E:/nastilna_gra_refactor/sounds;sounds/"',
-            '--add-data', '"E:/nastilna_gra_refactor/textures;textures/"',
-            '--add-data', '"E:/nastilna_gra_refactor/localization;localization/"',
-            '"E:/nastilna_gra_refactor/launch.py"',
-            ]
-
-    subprocess.call(args)
-
-    shutil.copytree(dest_folder / 'launch', compiled_out)
-
-    shutil.rmtree(dest_folder)
+        shutil.copytree(dist_folder / 'launch', compiled_out)
+    except Exception as e:
+        print(e)
+    else:
+        server_exe = dist_folder / 'server.exe'
+        server_exe_dst = compiled_out / 'server.exe'
+        subprocess.call('pyinstaller --noconfirm --onefile --console  "E:/nastilna_gra_refactor/server.py"')
+        shutil.copy(server_exe, server_exe_dst)
+    finally:
+        if dist_folder.exists():
+            shutil.rmtree(dist_folder)
+        if build_folder.exists():
+            shutil.rmtree(build_folder)
+        if launch_spec_file.exists():
+            os.remove(launch_spec_file)
+        if server_spec_file.exists():
+            os.remove(server_spec_file)
